4397ca2053a37ff299168c1f90cdea03624eafe9
Sérgio Matheus <matheus_imagina@hotmail.com>
2019-08-19 21:08:53 -0300

    adicao de metodos complementares

--- src/main/java/metric/LocClass.java
+++ src/main/java/metric/LocClass.java
@@ -1,33 +1,23 @@
 package metric;
-
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
-
+import java.lang.reflect.Method;
 public class LocClass {
 	public static void main(String[] args) {
-		String fileLocation = "C:\\Users\\100914500\\Desktop\\Metricas\\src\\main\\java\\metric\\LocClass.java";
-		Path path = FileSystems.getDefault().getPath(fileLocation);
-		
+//		String fileLocation = "C:\\Users\\100914500\\Desktop\\Metricas\\src\\main\\java\\metric\\LocClass.java";
+		String fileLocation = "C:\\Users\\Pichau\\Desktop\\metrics-evolucao\\src\\main\\java\\metric\\LocClass.java";
 		try {
-			System.out.println(countLines(fileLocation));
-//			System.out.println(countClassOfFile(fileLocation));
+			System.out.println("Linhas de codigo: "+countLines(fileLocation));
+			System.out.println("Número de Classes: "+countClass(fileLocation));
+			System.out.println("Número de Metodos: "+countMethods());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 	public static int countLines(String filename) throws IOException {
 		InputStream is = new BufferedInputStream(new FileInputStream(filename));
 		try {
@@ -48,5 +38,28 @@ public class LocClass {
 			is.close();
 		}
 	}
+	private static int countClass(String FILE_PATH) {
+		int classCount = 0;
+		try {				
+			String regex = "(public class|private class|protected class).*";
+			BufferedReader br = new BufferedReader(new FileReader(FILE_PATH));
+			while (br.ready()) {
+				String linha = br.readLine();
 
+				if(linha.matches(regex)) {
+					classCount++;
+				}
+			}
+			br.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		return classCount;
+	}
+	
+	private static int countMethods() {
+		Class<LocClass> classe = LocClass.class;
+		Method[] method = classe.getDeclaredMethods();
+		return method.length;
+	}
 }
