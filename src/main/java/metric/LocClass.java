d5103cbff4197129f15093febf89f2f44b60733d
Sérgio Matheus <matheus_imagina@hotmail.com>
2019-08-30 20:46:22 -0300

    add directory recursive

--- src/main/java/metric/LocClass.java
+++ src/main/java/metric/LocClass.java
@@ -1,17 +1,44 @@
 package metric;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LocClass {
+	private static int somaLinhasCodigo = 0;
+	private static int somaClasses = 0;
+	private static int somaMethods = 0;
+	
 	public static void main(String[] args) {
-		String fileLocation = "C:\\Users\\Pichau\\Desktop\\metrics-evolucao\\src\\main\\java\\metric\\LocClass.java";
-		System.out.println("Linhas de codigo: " + countLines(fileLocation));
-		System.out.println("Número de Classes: " + countClass(fileLocation));
-		System.out.println("Número de Metodos: " + countMethods(fileLocation));
+		File pasta = new File("C:\\Users\\Pichau\\Desktop\\Dataset");
+		navegatorPasta(pasta);
+		
+//		String fileLocation = "C:\\Users\\Pichau\\Desktop\\metrics-evolucao\\src\\main\\java\\metric\\LocClass.java";
+		System.out.println("-------------------------------------Somatorio total do projeto -------------------------------------");
+		System.out.println("Linhas de codigo: " + somaLinhasCodigo);
+		System.out.println("Número de Classes: " + somaClasses);
+		System.out.println("Número de Metodos: " + somaMethods);
+	}
+
+	private static void navegatorPasta(File pasta) {
+		File[] listaArquivos = pasta.listFiles();
+		for (int i = 0; i < listaArquivos.length; i++) {
+			if (listaArquivos[i].isDirectory()) {
+				navegatorPasta(listaArquivos[i]);
+			} else{
+				String fileLocation = listaArquivos[i].getPath();
+				somaLinhasCodigo += countLines(fileLocation);
+				somaClasses += countClass(fileLocation);
+				somaMethods += countMethods(fileLocation);
+				System.out.println("Diretório do arquivo: " + listaArquivos[i].getPath());
+				System.out.println("Linhas de codigo: " + countLines(fileLocation));
+				System.out.println("Número de Classes: " + countClass(fileLocation));
+				System.out.println("Número de Metodos: " + countMethods(fileLocation));
+			}
+		}
 	}
 
 	public static int countLines(String fileLocation) {
@@ -37,11 +64,11 @@ public class LocClass {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fileLocation));
 			final String regex = "(public|private|protected).*(class).*(\\()*(\\{)";
-
+			Pattern p = Pattern.compile(regex);
 			while (br.ready()) {
 				String linha = br.readLine();
-
-				if (linha.matches(regex)) {
+				Matcher m = p.matcher(linha);
+				if (m.find()) {
 					classCount++;
 				}
 			}
